e7f17308c261810bfa367e415c2b9e27c1015e21
abdul arif <23533098+aharif@users.noreply.github.com>
2019-04-26 00:25:36 -0700

    Update Script3.ps1

--- Assignment 8: PowerShell Report/Script3.ps1
+++ Assignment 8: PowerShell Report/Script3.ps1
@@ -1,1 +1,1 @@
-﻿Get-ADComputer -Identity 'PC01' -Properties * |select Name, Enabled, whenCreated, whenChanged, DistinguishedName | Export-CSV ADComputersList.csv -NoTypeInformation -Encoding UTF8
+﻿Get-ADComputer -Filter * -Properties * |select Name, Enabled, whenCreated, whenChanged, DistinguishedName | Export-CSV ADComputersList.csv -NoTypeInformation -Encoding UTF8

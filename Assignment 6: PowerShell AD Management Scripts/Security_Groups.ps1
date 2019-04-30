cbdd1edd9dcbff1e4a6b3b63c19161cb97c741c6
abdul arif <23533098+aharif@users.noreply.github.com>
2019-04-29 23:02:00 -0700

    Add files via upload

---
+++ Assignment 6: PowerShell AD Management Scripts/Security_Groups.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module ActiveDirectory
+$CSV = $args[0]
+$CSV = Import-Csv "$CSV"
+ForEach ($item In $CSV){
+	New-ADGroup -Name $item.name -Description $item.description -GroupCategory $item.category -GroupScope $item.scope -Path $item.oupath
+}
